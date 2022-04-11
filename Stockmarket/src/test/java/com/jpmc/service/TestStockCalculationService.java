eadc7840515fae51a3198c6a2e1e16e1332a792f
Sudipta Das <sudipta@Sudiptas-MacBook-Pro.local>
2022-04-11 17:15:02 +0530

    Addition#6

---
+++ Stockmarket/src/test/java/com/jpmc/service/TestStockCalculationService.java
@@ -0,0 +1,28 @@
+package com.jpmc.service;
+
+import com.jpmc.app.dao.StockServiceDAO;
+import com.jpmc.app.dataobjects.StockInfo;
+import com.jpmc.app.exception.ApplicationException;
+import com.jpmc.app.service.imp.StockCalculatorService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class TestStockCalculationService {
+
+    @MockBean
+    StockServiceDAO stoDao;
+
+    @Test(expected = ApplicationException.class)
+    public void test_pe_calculator_with_exception() throws ApplicationException {
+        when(stoDao.fetchStock(any(String.class))).thenReturn(new StockInfo());
+        StockCalculatorService service = new StockCalculatorService();
+        service.calculatePERatio("ABC",0);
+    }
+    
+}
