8e6ee0bce9b231a9f7a70bec5bdf69f8e89d84aa
Sudipta Das <sudipta@Sudiptas-MacBook-Pro.local>
2022-04-11 16:55:02 +0530

    Addition #4

---
+++ Stockmarket/src/test/java/com/jpmc/service/TestPreferredDividendCalculator.java
@@ -0,0 +1,24 @@
+package com.jpmc.service;
+
+import com.jpmc.app.dataobjects.StockInfo;
+import com.jpmc.app.service.imp.PrefferedTypeDividendCalculator;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class TestPreferredDividendCalculator {
+
+
+    @Test
+    public void test_preferred_stock(){
+        StockInfo info = new StockInfo();
+        info.setLastDividend(5.0);
+        info.setPer_value(100);
+        info.setFixedDividend(3);
+        PrefferedTypeDividendCalculator calculator = new PrefferedTypeDividendCalculator();
+        double result = calculator.calculateDividendYield(info,100);
+        Assert.assertEquals(3,result);
+    }
+}
